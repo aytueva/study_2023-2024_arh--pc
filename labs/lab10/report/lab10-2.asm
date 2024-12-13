b52c1af6a21292cf755279bf07a980c2ed129e3e
aytueva <1132242910@pfur.ru>
2024-12-13 17:29:11 +0300

    Add files via upload

---
+++ labs/lab10/report/lab10-2.asm
@@ -0,0 +1,47 @@
+%include 'in_out.asm'
+
+SECTION .data
+    filename db 'readme3.txt', 0h
+    msg1 db 'Как Вас зовут?', 0h
+    msg2 db 'Меня зовут ', 0h
+
+SECTION .bss
+    name resb 255
+
+SECTION .text
+global _start
+_start:
+    mov eax, msg1
+    call sprint
+
+    mov ecx, name
+    mov edx, 255
+    call sread
+
+    mov eax, 8
+    mov ebx, filename
+    mov ecx, 0744o
+    int 80h
+
+    mov esi, eax
+
+    mov eax, msg2
+    call slen
+    mov edx, eax
+    mov ecx, msg2
+    mov ebx, esi
+    mov eax, 4
+    int 80h
+
+    mov eax, name
+    call slen
+    mov edx, eax
+    mov ecx, name
+    mov ebx, esi
+    mov eax, 4
+    int 80h
+
+    mov ebx, esi
+    mov eax, 6
+    int 80h
+    call quit
